4d90325f318802542aadafe5cc364b178279dddf
azacdev <azacdev@gmail.com>
2023-06-20 22:23:41 +0100

    title fixed

--- src/features/posts/postSlice.jsx
+++ src/features/posts/postSlice.jsx
@@ -4,7 +4,7 @@ import { sub } from 'date-fns';
 const initialState = [
   {
     id: 1, 
-    title: 'Learning Redex Toolkit',
+    title: 'Learning Redux Toolkit',
     content: "I've heard good things",
     date: sub(new Date(), { minutes: 10 }).toISOString(),
     reactions: {
@@ -16,8 +16,8 @@ const initialState = [
     }  
   },
   {
-    id: 2, 
-    title: 'Slices', 
+    id: 2,
+    title: 'Slices',
     content: "The more i say slice, the more i want pizza",
     date: sub(new Date(), { minutes: 5 }).toISOString(),
     reactions: {
