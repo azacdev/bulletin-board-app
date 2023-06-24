285f28c7080f59e50b55ec19ddfbfd1fd7cb099e
azacdev <azacdev@gmail.com>
2023-06-24 21:37:12 +0100

    userSlice update

--- src/features/users/usersSlice.jsx
+++ src/features/users/usersSlice.jsx
@@ -1,17 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-  {id: '0', name: "Frank"},
-  {id: '1', name: "Henry"},
-  {id: '2', name: "Robbert"}
-]
+  { id: "0", name: "Frank" },
+  { id: "1", name: "Henry" },
+  { id: "2", name: "Robbert" },
+];
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
-  reducers: {}
-})
+  reducers: {},
+});
 
-export const selectAllUsers = (state) => state.users
+export const selectAllUsers = (state) => state.users;
 
-export default usersSlice.reducer
+export default usersSlice.reducer;
